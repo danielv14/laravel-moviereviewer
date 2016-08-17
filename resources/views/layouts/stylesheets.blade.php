697dddaff167e8fd9c97fcd4fc010c6fc9d8bbaf
Daniel <daniel.vernberg@gmail.com>
2016-08-17 15:37:34 +0200

    import google fonts

--- resources/views/layouts/stylesheets.blade.php
+++ resources/views/layouts/stylesheets.blade.php
@@ -1,2 +1,5 @@
 <link rel="stylesheet" href="/css/app.css" media="screen" title="no title" charset="utf-8">
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+{{-- Google fonts --}}
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
