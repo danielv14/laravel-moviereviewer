f51e39c75eba2661e6b125e6ebe478f7e8b1ceb3
Daniel <daniel.vernberg@gmail.com>
2016-08-22 10:24:04 +0200

    slower fade-out

--- public/js/all.js
+++ public/js/all.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
   // timed function to fade out the flash messages
   setTimeout(function() {
-    // target notice_wrapper div and fade it out after 2,5 sec
+    // target div and fade it out 
     $('#flash-wrapper').fadeOut("slow", function() {
       $(this).remove();
     })
-  }, 2500);
+  }, 3000);
 });
 
 //# sourceMappingURL=all.js.map
